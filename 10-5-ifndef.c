ce38e26fb7a5f48c36d091ea2f27ce4d1a0f0ab5
王家伟 <572264287@qq.com>
2019-01-21 21:38:25 +0800

    Create 10-5-ifndef.c

---
+++ 10-5-ifndef.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define STR  "diligence is the parent of success\n"
+
+int main(void) { 
+      
+    #ifdef STR
+    printf(STR);
+    #else
+    printf("idleness is the root of all evil\n");
+    #endif
+    
+    printf("\n");
+    
+    #ifndef ABC
+    printf("idleness is the root of all evil\n");
+    #else
+    printf(STR);
+    #endif
+
+	return 0;
+}
