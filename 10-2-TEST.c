fd259a1e578a6798d2a9900693f23e55bcf14bd9
王家伟 <572264287@qq.com>
2019-01-21 20:52:59 +0800

    Create 10-2-TEST.c

---
+++ 10-2-TEST.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define TEST "this is an example"
+
+int main(void) { 
+      
+      char exp[30] = "This TEST is not that TEST";
+       
+       printf("%s\n", exp);
+        
+    
+
+	return 0;
+}
