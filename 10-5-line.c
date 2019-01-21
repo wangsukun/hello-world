f0c5cf6b003bc89da7ce1ec5ed8adcb8aedce7da
王家伟 <572264287@qq.com>
2019-01-21 21:44:15 +0800

    Create 10-5-line.c

---
+++ 10-5-line.c
@@ -0,0 +1,11 @@
+#line 100 "13.7.C"
+#include <stdio.h>
+
+
+int main(void) { 
+      
+   printf("1.The current line NO.:%d\n", __LINE__);
+   printf("2.The current line NO.:%d\n", __LINE__);
+
+	return 0;
+}
