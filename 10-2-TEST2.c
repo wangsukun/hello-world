1bfb80bdf7b0bac5983ae9132b16e5b38eef1cbc
王家伟 <572264287@qq.com>
2019-01-21 20:55:38 +0800

    Create 10-2-TEST2.c

---
+++ 10-2-TEST2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define TEST "this is an example"
+
+int main(void) { 
+      
+      
+       
+       printf(TEST);
+       #undef TEST
+       
+       printf(TEST);
+        
+    
+
+	return 0;
+}
