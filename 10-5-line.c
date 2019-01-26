a3d73d4bc1ff0cb2de470635ab3abf6d98e35d45
王家伟 <572264287@qq.com>
2019-01-26 11:41:09 +0800

    Update 10-5-line.c

--- 10-5-line.c
+++ 10-5-line.c
@@ -6,6 +6,8 @@ int main(void) {
       
    printf("1.The current line NO.:%d\n", __LINE__);
    printf("2.The current line NO.:%d\n", __LINE__);
+   
+   printf("3.The current file name is:%s\n", __FILE__);
 
 	return 0;
 }
